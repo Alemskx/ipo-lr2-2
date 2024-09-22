b458ba3f57f4d83aaae8c1b94b3a9d9284c3ee37
Alemskx <alemskx@gmail.com>
2024-09-22 19:30:39 +0300

    Задание 2

---
+++ main.py
@@ -0,0 +1,6 @@
+n = int(input("Введи количество детей"))
+k = int(input("Введи количество яблок"))
+apple = k / n
+ostatok = k - apple * n
+print("По",apple,"достанеться каждому школьнику")
+print(ostatok,"останется в корзине")
